d13a9603362c1e4e0bf8f224fb6caf945c91ca06
Sanket-09 <f20200356@hyderabad.bits-pilani.ac.in>
2023-07-01 23:29:20 +0530

    day 5

---
+++ day5/sort_a_stack.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void sortStack(stack<int> &stack)
+{
+	vector<int>temp;
+
+    while(!stack.empty())
+    {
+        temp.push_back(stack.top());
+        stack.pop();
+    } 
+
+    sort(temp.begin(),temp.end());
+
+    for(int i =0;i<temp.size();i++)
+    {
+        stack.push(temp[i]);
+    }
+
+}
+
